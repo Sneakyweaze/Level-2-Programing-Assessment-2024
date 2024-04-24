ed91c633fed470411e2ccabbd084cec1b3259ed6
Sneakyweaze <126821969+Sneakyweaze@users.noreply.github.com>
2024-04-24 04:36:14 +0000

    added 20% of the pizza_order funtion

--- main.py
+++ main.py
@@ -6,12 +6,13 @@ pizzas_ = [[1, "Pepperoni", "$8.50"],
            [5, "Chicken Bacon & Aioli","$8.50"],
            [6, "Veggie","$8.50"],
            [7, "Hawaiian","$8.50"],
-           [8, "Plain Cheese","$5"],
+           [8, "Plain Cheese","$5"], 
            [9, "Greek Lamb","$5"],
            [10, "Garlic Prawn","$5"],
            [11, "Spicy Pepper","$5"],
            [12, "Ham & Cheese","$5"]]
 
+pizza_chosen = []
 #this varible sets the code to loop or not
 loop = True
 
@@ -24,18 +25,29 @@ def pizza_menu():
   1 - Pepperoni - $8.50
   2 - Meat Lovers - $8.50
   3 - Margherita - $8.50
-  ect. until the 2D list is finished
+  ect. until the 2D list is finished, meaning that you can add and remove pizzas from the list.
   '''
   print("These Are the Pizzas we sell:")
   for pizza in pizzas_:
-    print(pizza[0],'-','- '.join(pizza[1:3]))
+    print(pizza[0],'-',' - '.join(pizza[1:3]))
 
 
 
 def pizza_order():
-  print("test")
+  user_pizza = input("What pizza would you like to order?")
+  for pizza in pizzas_:
+    if user_pizza.lower() == pizza[1].lower():
+      print(f"You ordered a {pizza[1]} pizza.")
+      pizza_chosen.append(pizza[1:3])
+      break
+    elif user_pizza.lower() not in [pizza[1].lower() for pizza in pizzas_]:
+      print("Sorry, we don't have that pizza.")
+      break
+
+
 def pizza_checkout():
-  print("test")
+  for pizza in pizza_chosen:
+    print(pizza[0],'-' ,' - '.join(pizza[0:2]))
 def pizza_payment():
   print("test")
 
@@ -44,14 +56,17 @@ print("Welcome to the Pizza Shop")
 while loop is True:
   menu_select = input("Please Select an Option:\n1 - Pizza Menu\n2 - Add Pizza To Order\n3 - Checkout\n4 - Exit\n-> ")
   if menu_select == "1":
+    print("\n")
     pizza_menu()
     print("\n")
     continue
   elif menu_select == "2":
+    print("\n")
     pizza_order()
     print("\n")
     continue
   elif menu_select == "3":
+    print("\n")
     pizza_checkout()
     print("\n")
     continue 
@@ -59,5 +74,6 @@ while loop is True:
     print("Thank you for visiting the Pizza Shop.")
     loop = False
   else:
+    print("\n")
     print("Please Select a Valid Option")
     print("\n")
